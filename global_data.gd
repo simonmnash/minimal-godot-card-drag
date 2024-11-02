affdd3d7c59b12e4a6155becbc1c6e2d3f57e518
simonmnash <simon.m.nash@gmail.com>
2024-11-02 15:15:08 -0400

    fix export issues - instead of loading resources using DirAccess - make all_cards an @export and set imported cards manually.

--- global_data.gd
+++ global_data.gd
@@ -10,34 +10,13 @@ signal draw_card
 signal focus_ship (s : ShipData)
 signal card_up(c : CardView)
 signal select_card(c : CardView)
-var all_cards : Array[CardData]
+@export var all_cards : Array[CardData]
 var all_ships : Dictionary # Key: String (ship_class), Value: ShipData
 var players : Dictionary
 var focused_ship
 var local_player_team : Team
 
-func load_resource_folder(path='res://Data/Cards/') -> Array[CardData]:
-	var scene_loads : Array[CardData] = []
-	var dir = DirAccess.open(path)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
-		while file_name != "":
-			if dir.current_is_dir():
-				print("Found directory: " + file_name)
-			else:
-				if file_name.get_extension() == "tres":
-					var full_path = path.path_join(file_name)
-					print(full_path)
-					scene_loads.append(load(full_path))
-			file_name = dir.get_next()
-	else:
-		print("An error occurred when trying to access the path.")
-	return scene_loads
-
 func _ready():
-	all_cards = load_resource_folder()
-	# Build ships dictionary from cards
 	for card in all_cards:
 		all_ships[card.ship.ship_class] = card.ship
 		print(all_ships[card.ship.ship_class])
